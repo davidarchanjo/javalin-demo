fb92f9f6e7cee8196a221c24ed7a422b6620a1dc
David Archanjo <david.archanjo.ext@dasa.com.br>
2022-06-02 06:39:59 -0300

    first commit

--- src/main/java/io/davidarchanjo/JavalinDemoApplication.java
+++ src/main/java/io/davidarchanjo/JavalinDemoApplication.java
@@ -5,7 +5,7 @@ import io.javalin.Javalin;
 public class JavalinDemoApplication {	
 
 	public static void main(String[] args) {
-        Javalin app = Javalin.create();
+		Javalin app = Javalin.create();
 		app.get("/", ctx -> ctx.result("Hello World from Javalin!"));
 		app.start(8080);
     }
